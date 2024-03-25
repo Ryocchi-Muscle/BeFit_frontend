f52167e8ff3651e91e601fb9f98b7522e2695230
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-25 19:24:46 +0900

    Header.tsxを修正

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 import { type Session } from "next-auth";
-import Link from "next/link"; // Next.jsのLinkコンポーネントをインポート
 import { signOut } from "next-auth/react";
-import { signIn } from "next-auth/react";
-import Login from "./Login";
-
 
 const Header = ({ session }: { session: Session | null }) => {
-  const guestLogin = () => {
-    console.log("ゲストとしてログイン");
-  };
-
   return (
-    <header className="flex items-center justify-between bg-blue-950 p-4 shadow-md">
+    <header className="flex items-center justify-between bg-white p-4 shadow-md">
       <div className="flex items-center">
         <Link href="/" className="text-4xl font-bold">
-          FitApp
+          あなたのイケてるサービス
         </Link>
       </div>
       <ul className="flex items-center space-x-4">
@@ -35,7 +27,7 @@ const Header = ({ session }: { session: Session | null }) => {
             </li>
             <li>
               <button
-                onClick={() => signOut({ callbackUrl: "/" })}
+                onClick={() => signOut()}
                 className="rounded-lg bg-blue-500 px-4 py-[7px] text-white hover:bg-gray-600"
               >
                 ログアウト
@@ -43,23 +35,17 @@ const Header = ({ session }: { session: Session | null }) => {
             </li>
           </>
         ) : (
-          <>
-            <li>
-
-
-            </li>
-            {/* <li>
-              <Link href="/login">
-                <button className="rounded-lg bg-blue-500 px-4 py-[7px] text-white hover:bg-gray-600">
-                  ログイン
-                </button>
-              </Link>
-            </li> */}
-            <Login />
-          </>
+          <li>
+            <Link href="/login">
+              <button className="rounded-lg bg-blue-500 px-4 py-[7px] text-white hover:bg-gray-600">
+                ログイン
+              </button>
+            </Link>
+          </li>
         )}
       </ul>
     </header>
   );
 };
+
 export default Header;
