4c2850a5f8942ca67cc2fa95afdaac0e0bb12f44
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-28 07:13:11 +0900

    MenuComponent

--- src/app/category/task/page.tsx
+++ src/app/category/task/page.tsx
@@ -5,13 +5,14 @@ import DateCounter from "@/app/components/task/DateCounter";
 import ExerciseForm from "@/app/components/task/MenuComponent";
 import SetControlButtons from "@/app/components/task/SetControlButtons";
 import TrainingMenu from "@/app/components/task/TrainingMenu";
+import MenuComponent from "@/app/components/task/MenuComponent";
 
 export default function MissionPage() {
   return (
     <div>
       <DateCounter />
       <TodoComponent></TodoComponent>
-      <ExerciseForm></ExerciseForm>
+      <MenuComponent  number={10}/>
       <SetControlButtons></SetControlButtons>
       <h1>日々のミッション</h1>
       <TrainingMenu />
