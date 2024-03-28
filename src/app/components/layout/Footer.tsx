8e863ffa12816a3a2c6e8228dd30bf0c02268846
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-28 21:58:24 +0900

    fix classNameType React.ReactNode; to string;

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 type Props = {
   children?: React.ReactNode;
-  className?: React.ReactNode;
+  className?: string;
 };
 
 const Footer: React.FC<Props> = ({ children, className }) => {
