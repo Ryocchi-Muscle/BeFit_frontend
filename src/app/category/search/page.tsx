65ee0250cb5915fab4c4be9f46137a397f328ea5
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-03-30 21:16:26 +0900

    fix div's className

--- src/app/category/search/page.tsx
+++ src/app/category/search/page.tsx
@@ -5,11 +5,13 @@ import Footer from "@/app/components/layout/Footer";
 
 export default function MissionPage() {
   return (
-    <div>
-      <h1>動画検索</h1>
-      <p>ここに日々のミッションに関する内容を表示します。</p>
-      <Link href="/">ホームに戻る</Link>
-      <Footer />
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <h1>動画検索</h1>
+        <p>ここに日々のミッションに関する内容を表示します。</p>
+        <Link href="/">ホームに戻る</Link>
+        <Footer />
+      </div>
     </div>
   );
 }
