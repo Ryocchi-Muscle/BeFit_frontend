8a4047665a21ec0f1c79509358e429668974cb86
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-01 17:17:23 +0900

    delte jwt

--- src/app/utils/next-auth-options.ts
+++ src/app/utils/next-auth-options.ts
@@ -16,30 +16,6 @@ export const nextAuthOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    // jwt: async ({ token, user, account, profile }) => {
-    //   // 注意: トークンをログ出力してはダメです。
-    //   console.log("in jwt", { user, token, account, profile });
-    //   if (user) {
-    //     token.user = user;
-    //     const u = user as any;
-    //     token.role = u.role;
-    //   }
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //   }
-    //   return token;
-    // },
-    // session: ({ session, token }) => {
-    //   console.log("in session", { session, token });
-    //   token.accessToken;
-    //   return {
-    //     ...session,
-    //     user: {
-    //       ...session.user,
-    //       role: token.role,
-    //     },
-    //   };
-    // },
     async signIn({ user, account }) {
       const provider = account?.provider;
       const uid = user?.id;
