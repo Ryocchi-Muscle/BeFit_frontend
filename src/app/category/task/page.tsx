b28e5c7e71d99977f631557791e463068054d77e
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-08 19:29:27 +0900

    add TrainingDayComponent

--- src/app/category/task/page.tsx
+++ src/app/category/task/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "@/app/components/layout/Footer";
 import AddTrainigMenu from "@/app/components/task/AddTrainigMenu";
+import TrainingDayComponent from "@/app/components/task/TrainingDayComponent";
 
 export default function TaskPage() {
   return (
     <div className="flex flex-col h-screen">
       <div className="flex-1 overflow-y-auto">
         <h1 className="text-4xl">日々のミッション</h1>
+        <TrainingDayComponent />
         <AddTrainigMenu />
       </div>
       <Footer className="w-full" />
