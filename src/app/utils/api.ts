4c01ff123b649078d05cc1a52fba9b77d059c59b
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-12 21:00:46 +0900

    create file

---
+++ src/app/utils/api.ts
@@ -0,0 +1,12 @@
+// import { getSession } from "next-auth/react";
+// interface Session{
+//   accessToken: string;
+// }
+
+
+// export async function secureApiCall(endpoint, method = "GET", body = null) {
+//   const session = await getSession() as Session;
+//   const token = session?.accessToken;
+
+//   return ;
+// }
