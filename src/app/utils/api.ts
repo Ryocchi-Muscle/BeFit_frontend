a44bf921a39bba4bcd910eefe3820067b46497b9
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-12 21:45:15 +0900

    共通のAPI呼び出し関数の実装

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,12 +1,21 @@
-// import { getSession } from "next-auth/react";
-// interface Session{
-//   accessToken: string;
-// }
+import { getSession } from "next-auth/react";
 
+export async function secureApiCall(
+  endpoint: string,
+  method: "GET" | "POST" = "GET",
+  body?: object
+) {
+  const session = await getSession();
+  const token = session?.accessToken;
 
-// export async function secureApiCall(endpoint, method = "GET", body = null) {
-//   const session = await getSession() as Session;
-//   const token = session?.accessToken;
-
-//   return ;
-// }
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+   const response = await fetch(endpoint, {
+    method: method,
+    headers: headers,
+    body: JSON.stringify(body)
+  });
+  return response;
+}
