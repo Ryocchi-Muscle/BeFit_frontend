b7899fcc8bc61098d7cf7ae24571364949daddf6
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-13 15:48:05 +0900

    create function

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,21 +1,40 @@
 import { getSession } from "next-auth/react";
+import { ApiResponse } from "../../../types/api";
 
-export async function secureApiCall(
+// APIを呼び出す関数の定義
+export async function secureApiCall<T>(
   endpoint: string,
   method: "GET" | "POST" = "GET",
   body?: object
-) {
+): Promise<ApiResponse<T>> {
+  //セッションと認証トークンの取得
   const session = await getSession();
   const token = session?.accessToken;
 
-  const headers = {
+  // ヘッダーの設定;
+  const headers = new Headers({
     Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
-  };
-   const response = await fetch(endpoint, {
-    method: method,
-    headers: headers,
-    body: JSON.stringify(body)
   });
-  return response;
+
+  // リクエスト設定
+  const config: RequestInit = {
+    method,
+    headers,
+    body: body ? JSON.stringify(body) : null,
+  };
+  // GETメソッドではボディが不要なため、ボディを削除してる。
+  if (method === "GET") {
+    delete config.body;
+  }
+  // API呼び出しとエラーハンドリング
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/${endpoint}`,
+    config
+  );
+  if (!response.ok) {
+    throw new Error(`API call failed with status: ${response.status}`);
+  }
+  const data: T = await response.json();
+  return { data };
 }
