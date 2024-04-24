9ef5e8ca0e3f203447f671b156d53bdb2556ab09
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-24 22:11:38 +0900

    add

--- src/components/ui/calender.tsx
+++ src/components/ui/calender.tsx
@@ -15,8 +15,9 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/app/features/calender/CalendarDialog";
-
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
