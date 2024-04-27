e5eba6f6d459eb44ec086fc2feaf5742283f713c
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-04-27 10:51:43 +0900

    change directory's name v2

--- src/components/ui/calender.tsx
+++ src/components/ui/calender.tsx
@@ -19,7 +19,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import TrainingRecord from "@/app/components/CarendarRecord/v1/TrainingRecord";
-import TrainingForm from "@/app/components/CarendarRecord/TrainingFrom(modal)";
+import TrainingForm from "@/app/components/CarendarRecord/v2/TrainingFrom(modal)";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
