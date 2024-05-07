be5f3097bcb23788ddfc8356210ed380381a626f
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-07 20:21:50 +0900

    delete calendar component

--- src/app/category/record/page.tsx
+++ src/app/category/record/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 // import TodoComponent from "@/app/components/todo/TodoList";
 import Footer from "@/app/components/layout/Footer";
-import { Calendar } from "@/components/ui/calender";
 import React from "react";
 
 function RecordPage() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
         <h1 className="text-4xl ">記録</h1>
       </div>
+
       <Footer />
     </div>
   );
