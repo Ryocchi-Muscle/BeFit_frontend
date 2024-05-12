0919dfe1e3d1890fc397e0a173f205a59cf9fc1d
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-12 21:51:08 +0900

    add toast

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -5,8 +5,36 @@ import React from "react";
 import { type Session } from "next-auth";
 import Link from "next/link"; // Next.jsのLinkコンポーネントをインポート
 import { signOut } from "next-auth/react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const Header = ({ session }: { session: Session | null }) => {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      // signOut関数をawaitで待つ
+      await signOut({ redirect: false }); // リダイレクトを自動で行わないように設定
+      // ログアウト成功時のトースト通知
+      toast({
+        title: "ログアウトしました。",
+        style: { backgroundColor: "green", color: "white" },
+        duration: 3000,
+      });
+      // 成功後、手動でホームにリダイレクト
+      router.push("/");
+    } catch (error) {
+      // ログアウト失敗時のトースト通知
+      toast({
+        title: "エラーが発生しました。",
+        description: "ログアウトに失敗しました。もう一度お試しください。",
+        duration: 3000,
+      });
+      console.error("Logout failed:", error);
+    }
+  };
+
   return (
     <header className="fixed top-0 left-0 right-0 flex items-center justify-between bg-blue-950 shadow-md p-2 lg:p-4">
       <div className="flex items-center">
@@ -28,7 +56,7 @@ const Header = ({ session }: { session: Session | null }) => {
             </li>
             <li>
               <button
-                onClick={() => signOut({ callbackUrl: "/" })}
+                onClick={handleSignOut}
                 className="rounded-lg bg-blue-500 px-4 py-[7px] text-white hover:bg-gray-600"
               >
                 ログアウト
