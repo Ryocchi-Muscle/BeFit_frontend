205869ca55a8e3aa64c75b501b5c52e12faa0d7d
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-05-12 10:04:04 +0900

    fix setting's imageUrl

--- src/app/category/user_info/page.tsx
+++ src/app/category/user_info/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Footer from "@/app/components/layout/Footer";
 import UserProfile from "@/app/components/UserProfile/UserProfile";
+import { useSession } from "next-auth/react";
 
 type GoogleData = {
   name: string;
@@ -17,8 +20,18 @@ const googleData = {
   imageUrl: "https://via.placeholder.com/150",
 };
 
-export default function userInfoPage() {
-  const userId = "1";
+export default function UserInfoPage() {
+  const { data: session, status } = useSession();
+  if (status === "loading") return <div>Loading...</div>;
+
+  if (!session || !session.user) {
+    return <p>Please login to view this page.</p>;
+  }
+
+  const googleData = {
+    name: session.user.name || "", // ユーザー名が無ければデフォルト値を使用
+    imageUrl: session.user.image || "https://via.placeholder.com/150", // セッションから画像URLを取得、無ければデフォルト画像を使用
+  };
 
   return (
     <div className="flex flex-col min-h-screen">
