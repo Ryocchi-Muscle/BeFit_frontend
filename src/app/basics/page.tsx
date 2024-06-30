2e76795d9ca4bf27b98a6b64242bd67998323c87
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-06-30 17:18:13 +0900

    create ComingSoon's Component

---
+++ src/app/basics/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComingSoon from "../components/ComingSoon/ComimgSoon";
+
+const BasicPage: React.FC = () => {
+  return (
+    <>
+      <ComingSoon/>
+    </>
+  );
+};
+
+export default BasicPage;
