abffb377258b79c5e104d49ce5fe4c3b9ea09a15
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2024-07-03 11:25:43 +0900

    ログイン時の表示コンポーネントを固定

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-xs space-y-6 rounded-3xl bg-white p-8 shadow-xl absolute top-20">
+      <div className="w-full max-w-xs space-y-6 rounded-3xl bg-white p-8 shadow-xl fixed top-20">
         <div className="text-center">
           <Image
             src="/images/Pasted Graphic.png"
